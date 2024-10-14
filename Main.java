90e0e7e7b4cbc7c7b3d773fff3192a8c4c6d1287
Karuna Kumari <mishrakaruna9142@gmail.com>
2024-10-14 23:17:40 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,47 +1,55 @@
 package com.jdbc;
-import java .sql.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-	
-		String url = "jdbc:mysql://localhost:3306/jdbc";
-		String username = "root";
-		String password = "Mishra12345#";
-		
-		
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-		} catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-        
-		try {
-			Connection con = DriverManager.getConnection(url, username, password);
-		     System.out.println("connection is done..");
-			Statement statement = con.createStatement();
-			//query
-			String query = "select * from student";
+public static void main(String[] args) {
 			
-			//result
-			ResultSet resultset = statement.executeQuery(query);
-			while(resultset.next()){
-				int id = resultset.getInt("id");
-				String name = resultset.getString("name");
-				int age = resultset.getInt("age");
-				
-				System.out.println("student id= "+id);
-				System.out.println("student name= "+name);
-				System.out.println("student age= "+age);
-			}
-		} 
-		catch(SQLException e)
-		{
-			e.printStackTrace();
-		}
 		
+			String url = "jdbc:mysql://localhost:3306/jdbc";
+			String username = "root";
+			String password = "Mishra12345#";
+			
+			
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+			} catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+	        
+			try {
+				Connection con = DriverManager.getConnection(url, username, password);
+			     System.out.println("connection is done..");
+				Statement statement = con.createStatement();
+				//query
+				String query = "select * from student";
+				
+				//result
+				ResultSet resultset = statement.executeQuery(query);
+				while(resultset.next()){
+					int id = resultset.getInt("id");
+					String name = resultset.getString("name");
+					int age = resultset.getInt("age");
+					
+					System.out.println("student id= "+id);
+					System.out.println("student name= "+name);
+					System.out.println("student age= "+age);
+				}
+			} 
+			catch(SQLException e)
+			{
+				e.printStackTrace();
+			}
+			
+		}
+
 	}
 
-}
+
